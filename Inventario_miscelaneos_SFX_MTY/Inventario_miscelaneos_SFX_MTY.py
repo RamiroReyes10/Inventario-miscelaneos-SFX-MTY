cbcd4fc067f3ce37e8a0c4e91ef83b565fd95d86
Ramiro Reyes <ramiro_rey12@hotmail.com>
2024-04-11 15:54:29 -0600

    se agrega cambios de modulos en intransit y salidasmas la copartiva en las consultas

--- Inventario_miscelaneos_SFX_MTY/Inventario_miscelaneos_SFX_MTY.py
+++ Inventario_miscelaneos_SFX_MTY/Inventario_miscelaneos_SFX_MTY.py
@@ -1,5 +1,5 @@
 import tkinter
-from tkinter import ANCHOR, CENTER, CURRENT, Label, ttk
+from tkinter import ANCHOR, CENTER, CURRENT, LEFT, Label, ttk
 from tkinter import filedialog
 from tkinter.font import Font
 import pyodbc
@@ -82,11 +82,12 @@ def show_consultas():
     for widget in ventana_princ.winfo_children():
         widget.place_forget()
 
-    boton_consultas.place(x=130, y=95)
-    boton_volver.place(x=300, y=150)
+    boton_consultas.place(x=110, y=95)
+    boton_consuta_int.place(x=285, y=95)
+    boton_volver.place(x=370, y=150)
 
     titulo_consultas_reg = tk.Label(ventana_princ, text="Que registro deseas ver?")
-    titulo_consultas_reg.place(x=210, y=13)
+    titulo_consultas_reg.place(x=140, y=13)
     titulo_consultas_reg.config(bg="white",
                                 font=("Arial Black",12))
 
@@ -162,6 +163,17 @@ def show_consulta_accion():
     estado_prod.config(bg="white",
                          font=("Arial Black",8))
 
+def show_consulta_de_intransit():
+    for widget in ventana_princ.winfo_children():
+        widget.place_forget()
+
+        combo.place(x=200, y=35)
+        boton_selec_prod_in_int.place(x=50, y=35)
+        stock_int.place(x=200, y=95)
+        boton_consultar_in_intransit.place(x=250, y=150)
+        boton_volver_consultas.place(x=360, y=150)
+
+
 
 #funcion para guardar stock entrada
 def guardar_entrada():
@@ -189,6 +201,7 @@ def guardar_entrada():
 
             #este linea hace que se guarden los cambios en la base de datos
             connection.commit()
+            messagebox.showinfo("Exito", "Entrada guardada correctamente")
         except Exception as e:
             messagebox.showerror(f"Error: {e}")
     else:
@@ -197,10 +210,7 @@ def guardar_entrada():
 
 #funcion para guardar stock Salida
 def guardar_salida():
-    
-    #Mismo proceso para guardar entradas pero ahora restando en el query
     nombre_del_producto_sal = combo.get()
-
     cantidad_sal = entry.get()
     
     if nombre_del_producto_sal and cantidad_sal:
@@ -209,16 +219,24 @@ def guardar_salida():
             
             cursor.execute("UPDATE dbo.Inventarios SET stock = stock - ? WHERE Nombre_del_producto = ?", (cantidad_sal, nombre_del_producto_sal))
             
-            cursor.execute("SELECT stock FROM dbo.Inventarios WHERE Nombre_del_producto = ?",(nombre_del_producto_sal))
-            stock_actual = cursor.fetchone()[0]
-            
-            cursor.execute("INSERT INTO dbo.Salidas (nombre_del_producto, stock_actual, salida, fecha) VALUES (?, ?, ?, GETDATE())",(nombre_del_producto_sal, stock_actual, cantidad_sal,))
-
-            connection.commit()
+            cursor.execute("SELECT stock, Punto_Reorden FROM dbo.Inventarios WHERE Nombre_del_producto = ?",(nombre_del_producto_sal,))
+            resultado = cursor.fetchone()  
+            if resultado:  
+                stock_actual, punto_de_reorden = resultado
+                
+                cursor.execute("INSERT INTO dbo.Salidas (nombre_del_producto, stock_actual, salida, fecha) VALUES (?, ?, ?, GETDATE())",(nombre_del_producto_sal, stock_actual, cantidad_sal,))
+    
+                if stock_actual < punto_de_reorden:
+                    cursor.execute("UPDATE dbo.Inventarios SET Accion = 'Comprar' WHERE Nombre_del_Producto = ?", (nombre_del_producto_sal,))
+                    connection.commit()
+    
+                messagebox.showinfo("Exito", "Salida guardada correctamente")
+            else:
+                messagebox.showerror("Error", "Producto no encontrado en el inventario.")
         except Exception as e:
-            messagebox.showerror(f"Error: {e}")
+            messagebox.showerror("Error", f"Error: {e}")
     else:
-        messagebox.showerror("error", f"Por favor, selecciona un producto e ingresa una cantidad.")
+        messagebox.showerror("Error", "Por favor, selecciona un producto e ingresa una cantidad.")
 
 def guardar_intransit():
     nombre_del_producto_ints = combo.get()
@@ -227,11 +245,14 @@ def guardar_intransit():
     if nombre_del_producto_ints and cantidad_ints:
         try:
             cantidad_ints = int(cantidad_ints)
+            cursor.execute("UPDATE dbo.inventarios SET Accion = 'En Transito' WHERE Nombre_del_producto = ?", (nombre_del_producto_ints,))
             cursor.execute("UPDATE dbo.Inventarios SET intran = ISNULL(intran,0) + ? WHERE Nombre_del_producto = ?", (cantidad_ints, nombre_del_producto_ints))
             cursor.execute("Select stock FROM dbo.Inventarios where Nombre_del_producto = ?",(nombre_del_producto_ints))
             stock_actual = cursor.fetchone()[0]
             cursor.execute("INSERT INTO dbo.Intransit (nombre_del_producto, stock_actual, intransit, fecha) VALUES (?, ?, ?, GETDATE())",(nombre_del_producto_ints, stock_actual, cantidad_ints))
             connection.commit()
+            messagebox.showinfo("Exito", "Informacion guardada correctamente")
+
         except Exception as e:
             messagebox.showerror(f"Error: {e}")
     else:
@@ -250,6 +271,9 @@ def recibir_material_instransit():
             stock_actual = cursor.fetchone()[0]
             cursor.execute("INSERT INTO dbo.Intransit (nombre_del_producto, stock_actual, recibo_de_material, fecha) VALUES (?, ?, ?, GETDATE())",(nombre_de_producto_inst_rm, stock_actual, cantidad_ints_rm))
             connection.commit()
+
+            messagebox.showinfo("exito", "Informacion guardada correctamente")
+
         except Exception as e:
             messagebox.showerror(f"Error: {e}")
     else:
@@ -259,17 +283,29 @@ def actualizacion_stock_consulta():
     producto_seleccionado = combo.get()
     cursor.execute("SELECT stock, Accion FROM dbo.inventarios WHERE Nombre_del_producto = ?", (producto_seleccionado,))
     resultado = cursor.fetchone()
-    connection.commit()
 
     if resultado:
         stock_textbox.delete(0, tk.END) 
         stock_textbox.insert(0, resultado[0])  
 
         accion_textbox.delete(0, tk.END)  
-        accion_textbox.insert(0, resultado[1])  
+        accion_textbox.insert(0, resultado[1])
+ 
+    else:
+        messagebox.showerror("error", f"Producto no encontrado")
+
+def actualizacion_intransit_consulta():
+    producto_seleccionado = combo.get()
+    cursor.execute("SELECT intran FROM dbo.Inventarios WHERE Nombre_del_producto = ?", (producto_seleccionado,))
+    resultado = cursor.fetchone()
+    connection.commit()
+
+    if resultado:
+        stock_int.delete(0, tk.END)
+        stock_int.insert(0, resultado[0])
 
     else:
-        print("Producto no encontrado")
+        messagebox.showerror("Error", f"Producto no encontrado")
 
 
 def export_excel_inventario():
@@ -381,16 +417,19 @@ boton_registo_int = tk.Button(ventana_princ, text="Registros", command=export_ex
 boton_recibo_material = tk.Button(ventana_princ, text="Recibo de material", command=show_recibo_material_intransit)
 boton_guardar_recibo_material = tk.Button(ventana_princ, text="guardar", command=recibir_material_instransit)
 
+boton_selec_prod_in_int = tk.Button(ventana_princ, text="Cargar informacion", command=cargar_productos)
+boton_consultar_in_intransit = tk.Button(ventana_princ, text="Consultar", command=actualizacion_intransit_consulta)
+
 boton_guardar_ints =tk.Button(ventana_princ, text="Guardar", command=guardar_intransit)
 boton_volver_main_int =tk.Button(ventana_princ, text="volver", command=show_intransit_interfaz)
 
 titulo_consultas = tk.Label(ventana_princ, text="Consultas")
 boton_consultas = tk.Button(ventana_princ, text="Consulta de stock", command=show_consulta_accion)
+boton_consuta_int = tk.Button(ventana_princ, text="Consulta de Intrasit", command=show_consulta_de_intransit)
 boton_selec_prod_accion = tk.Button(ventana_princ, text="consultar producto", command=cargar_productos)
 boton_consultar_produc_accion = tk.Button(ventana_princ, text="Consultar", command=actualizacion_stock_consulta)
 boton_volver_consultas = tk.Button(ventana_princ, text="volver", command=show_consultas)
 accion = tk.Entry(ventana_princ)
-
 
 boton_reg_entradas = tk.Button(ventana_princ, text="Registro de entradas", command=export_excel_entradas)
 boton_reg_salidas = tk.Button(ventana_princ, text="Registro de Salidas", command=export_excel_salidas)
@@ -401,6 +440,9 @@ stock_textbox.pack(pady=5)
  
 accion_textbox = tk.Entry(ventana_princ)
 accion_textbox.pack(pady=5)
+
+stock_int = tk.Entry(ventana_princ)
+
 
 combo = ttk.Combobox(ventana_princ, state="readonly", font=Font(size=15))
 
